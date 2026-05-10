a0d48179efe179c2666f699253ed983ff51daf5c
M0rsus <shchepin1900@gmail.com>
2026-05-10 22:50:05 +0700

    class cleanup

--- Assets/Game/Scripts/IWeapon.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Game
-{
-    public interface IWeapon
-    {
-        void Fire();
-    }
-}
---
+++ Assets/Game/Scripts/UI/TempButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI
+{
+    public class TempButton : MonoBehaviour
+    {
+        [SerializeField] private GameObject spawn;
+        [SerializeField] private GameObject card;
+        public static event Action OnCardCreate;
+
+        public void CreateCard()
+        {
+            Instantiate(card, spawn.transform);
+            OnCardCreate?.Invoke();
+        }
+    }
+}
