678ed4fbfb632faa2685b442b60eb1b71cfa5eca
M0rsus <shchepin1900@gmail.com>
2026-06-07 23:02:36 +0700

    add spawners

---
+++ Assets/Game/Scripts/Enemies/Spawn.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class Spawn : MonoBehaviour
+    {
+        private GameObject[] _spawner;
+
+        private void Start()
+        {
+            int count = 0;
+            _spawner = new GameObject[transform.childCount];
+            foreach(Transform child in transform)
+            {
+                _spawner[count] = child.gameObject;
+                count++;
+            }
+        }
+        public void Instantiate(GameObject gameObject)
+        {
+            Instantiate(gameObject, _spawner[Random.Range(0, _spawner.Length)].transform.position, Quaternion.identity);
+        }
+    }
+}
