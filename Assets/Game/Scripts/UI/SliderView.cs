e0f78a389b36cea5c359a203a81287f489e56ee4
M0rsus <shchepin1900@gmail.com>
2026-06-14 23:05:40 +0700

    add hud

---
+++ Assets/Game/Scripts/UI/SliderView.cs
@@ -0,0 +1,25 @@
+﻿using Game;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class SliderView : MonoBehaviour
+    {
+        [SerializeField]
+        private Slider slider;
+        [SerializeField]
+        private TextMeshProUGUI text;
+
+        private StatInt _current;
+        private StatInt _max;
+
+        public void Initialize(StatInt current, StatInt max)
+        {
+            _current = current;
+            _max = max;
+        }
+        
+    }
+}
